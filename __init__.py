7e1ea79cefe40512b8b778f7cdb2aec7a490ef95
WanderingMouse <215439289+WanderingMouse@users.noreply.github.com>
2025-06-09 00:53:13 -0500

    Rename replace node to avoid conflict with ComfyUI base component

--- __init__.py
+++ __init__.py
@@ -2,20 +2,20 @@
 
 from .string_textbox_node import StringTextboxNode
 from .string_strip_node import StringStripNode
-from .string_replace_node import StringReplaceNode 
+from .string_multi_replace_node import StringMultiReplaceNode 
 from .string_preview_node import StringPreviewNode
 
 NODE_CLASS_MAPPINGS = {
     "StringTextbox": StringTextboxNode,
     "StringStrip": StringStripNode,
-    "StringReplace": StringReplaceNode,
+    "StringMultiReplace": StringMultiReplaceNode,
     "StringPreview": StringPreviewNode
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "StringTextbox": "String Textbox",
     "StringStrip": "String Strip",
-    "StringReplace": "String Replace",
+    "StringMultiReplace": "String Multi Replace",
     "StringPreview": "String Preview"
 }
 
